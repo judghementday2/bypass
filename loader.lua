caf924359c888d54687c36937f9c1d478442902c
judghementday2 <AnonymousPerson123ZK@protonmail.com>
2025-03-03 19:20:12 +0200

    Update loader.lua

--- loader.lua
+++ loader.lua
@@ -17,7 +17,7 @@ game_list['phantom forces'] = {
 };
 game_list['fallen survival'] = {
     status    = 'undetected',
-    executors = { 'AWP', 'Wave', 'Xeno' }
+    executors = { 'AWP', 'Wave' }
 };
 game_list['trident survival'] = {
     status    = 'updating',
